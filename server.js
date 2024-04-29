86facd99d4a98bcc47ef7c29e41cc2b1968f2199
bhupendrachouhan19 <iiitianbhupendra@gmail.com>
2024-04-30 04:08:07 +0530

    Exploring different types of API GET reponses

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get('/api/contacts/', (req, res) => {
+    // Differnt types of response:
+
+    // 1. Regular Message Response:
+    // res.send('Get all contacts.')
+    
+    // 2. Regular JSON Response:
+    // res.json({message: "This is the .json response for api/contacts"})
+    
+    // 3. JSON Response with custom status code:
+    res.status(230).json({message: "OK"}); // json response with custom status code '230'
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 })
